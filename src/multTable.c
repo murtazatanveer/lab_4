4c6380616932554754c5c74119e9b1b569c215b7
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-18 20:50:41 +0500

    9th in table.c

--- src/multTable.c
+++ src/multTable.c
@@ -8,12 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-
-  if(argc != 2) {
-    printf("Error: provide an integer\n");
-    exit(1);
-  }
+  
 
   int n = atoi(argv[1]);
 
